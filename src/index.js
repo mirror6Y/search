a26a7f70e134f6c43b63fcf5e1f528334c60a823
mirror6Y <1766650501@qq.com>
2020-05-29 16:56:22 +0800

    echarts

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import 'antd/dist/antd.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Search from './service/search'
 import Index from './component/index'
+// import ReactEcharts from './service/data';
+
+
 
 ReactDOM.render(
   <Index />,
+// < ReactEcharts />,
   document.getElementById('root')
 );
+
+
 
 
 serviceWorker.unregister();
